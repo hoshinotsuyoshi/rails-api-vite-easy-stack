fd6df247af4d64f4d16414bf36be91ad8c1aa160
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-09-30 05:16:29 +0900

    port

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -64,6 +64,6 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 10025 }
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 3025 }
   config.action_mailer.raise_delivery_errors = false
 end

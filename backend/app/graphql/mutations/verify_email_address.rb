f68dc38cb3f05ef3424cd2aa71bda1000461b9c8
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-09-30 05:51:10 +0900

    WIP

--- backend/app/graphql/mutations/verify_email_address.rb
+++ backend/app/graphql/mutations/verify_email_address.rb
@@ -7,7 +7,9 @@ module Mutations
     argument :signed_id, GraphQL::Types::String, required: true
 
     def resolve(signed_id:)
-      User.find_signed(signed_id, purpose: :invite)
+      user = User.find_signed(signed_id, purpose: :invite)
+      user && start_new_session_for(user)
+      user
     end
   end
 end

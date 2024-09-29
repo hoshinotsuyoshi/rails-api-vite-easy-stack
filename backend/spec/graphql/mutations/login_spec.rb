14f4a064c4ab8adf4257be2620adf18b5861f295
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-09-30 00:05:58 +0900

    WIP spec

---
+++ backend/spec/graphql/mutations/login_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Mutations::Login, type: :request do
+  describe '#resolve' do
+    subject { BackendSchema.execute(query_string, variables:, context:) }
+    let!(:query_string) do
+      <<~GRAPHQL
+        mutation ($loginInput: LoginInput!) {
+          login(input: $loginInput) {
+            id
+            emailAddress
+          }
+        }
+      GRAPHQL
+    end
+
+    let!(:context) { { } }
+    let!(:variables) do
+      {
+        loginInput: {
+          emailAddress: "alice@example.com",
+          password: "password",
+        }
+      }
+    end
+
+    context "when password is wrong" do
+      it "null User" do
+        expect { subject }.not_to change(Session, :count)
+        expect(JSON.parse(subject.to_json).deep_symbolize_keys).to eq(
+          data: {
+            login: nil
+          }
+        )
+      end
+    end
+  end
+end

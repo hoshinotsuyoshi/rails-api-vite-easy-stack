b65be0e5178c8650bf17d636635feb3a045ffd63
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-10-13 18:00:29 +0900

    WIP

--- frontend/src/components/Me/index.tsx
+++ frontend/src/components/Me/index.tsx
@@ -1,15 +1,18 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import type { User } from '../../generated/graphql'
+import { MeDocument } from '../../generated/graphql'
 import { ROUTES } from '../../routes'
-import { ME } from './ME_QUERY'
 
 export const Me = () => {
-  const me = useQuery<{ me?: User }>(ME)
-  const email = me.data?.me?.emailAddress
+  const { data } = useQuery(MeDocument)
+  const email = data?.me?.emailAddress
   const navigate = useNavigate()
-  email || navigate(ROUTES.LOGIN)
+
+  if (!email) {
+    navigate(ROUTES.LOGIN)
+  }
+
   return (
     <>
       {email && <div>user: {email}</div>}

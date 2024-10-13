b3a6dc5da4b2229c06b63c4c80ab46098823f33d
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-10-14 07:52:13 +0900

    WIP

--- frontend/src/components/Me/index.tsx
+++ frontend/src/components/Me/index.tsx
@@ -1,8 +1,8 @@
-import { useQuery } from '@apollo/client'
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { MeDocument, useMeQuery } from '../../generated/graphql'
+import { useMeQuery } from '../../generated/graphql'
 import { ROUTES } from '../../routes'
+import { LogoutButton } from './LogoutButton'
 
 export const Me = () => {
   const { data, loading } = useMeQuery()
@@ -20,6 +20,9 @@ export const Me = () => {
       {loading && <div>Loading...</div>}
       {email && <div>user: {email}</div>}
       {email && <div>hello, It's me!</div>}
+      <div>
+        <LogoutButton />
+      </div>
     </>
   )
 }

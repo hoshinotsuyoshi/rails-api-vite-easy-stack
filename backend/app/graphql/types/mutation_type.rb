cde5f5a92be5073d110b99154110f72c314a8491
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-10-13 23:06:54 +0900

    WIP

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,9 +2,10 @@
 
 module Types
   class MutationType < Types::BaseObject
-    field :login, mutation: Mutations::Login, null: true
-    field :set_password, mutation: Mutations::SetPassword, null: true
-    field :signup, mutation: Mutations::Signup, null: true
-    field :verify_email_address, mutation: Mutations::VerifyEmailAddress, null: true
+    field :login, mutation: Mutations::Login, null: false
+    field :logout, mutation: Mutations::Logout, null: false
+    field :set_password, mutation: Mutations::SetPassword, null: false
+    field :signup, mutation: Mutations::Signup, null: false
+    field :verify_email_address, mutation: Mutations::VerifyEmailAddress, null: false
   end
 end

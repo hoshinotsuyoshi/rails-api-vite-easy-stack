1c219ae95714041cc05f8ee4a62521c1e74f79a3
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-10-14 09:38:13 +0900

    test

--- backend/spec/system/scenarios/login_flow_spec.rb
+++ backend/spec/system/scenarios/login_flow_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "login flow", type: :system do
-  include ActiveJob::TestHelper
-
   before do
     driven_by(:headless_chrome)
   end
   let!(:user) { create(:user) }
 
-  it 'me -> login -> me' do
+  it 'me -> login -> me -> logout -> me -> login' do
     visit '/me'
 
     fill_in "email", with: user.email_address
@@ -16,7 +14,11 @@ RSpec.describe "login flow", type: :system do
     click_button "Login"
 
     expect(page).to have_content("hello, It's me!")
-    sleep 2
     expect(page).to have_content(user.email_address)
+    accept_alert do
+      click_button "Logout"
+    end   
+    expect(page).to have_content("Login")
+    expect(current_path).to eq("/login")
   end
 end

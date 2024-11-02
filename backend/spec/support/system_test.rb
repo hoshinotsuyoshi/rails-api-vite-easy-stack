81b0b828da846dfbdcffcc423f042db0a67a8fe7
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2024-11-02 12:59:19 +0900

    Update system tests to use only Chromium in CI
    - Modified CI workflow to install Playwright with Chromium support only, removing Firefox.
    - Updated `system_test.rb` to use Chromium exclusively for system tests.
    - Simplified browser configuration, eliminating random browser selection.

--- backend/spec/support/system_test.rb
+++ backend/spec/support/system_test.rb
@@ -3,12 +3,7 @@ require 'capybara/rspec'
 Capybara.server = :puma, { Silent: true }
 
 RSpec.configure do |config|
-  using = ENV['SYSTEM_TEST_BROWSER']&.to_sym
-
   config.before(:each, type: :system) do
-    using ||= [:chromium, :firefox].sample.tap do |driver|
-      puts "System test: #{driver} has been randomly selected."
-    end
-    driven_by :playwright, options: { headless: true, browser_type: using }
+    driven_by :playwright, options: { headless: true, browser_type: :chromium }
   end
 end
